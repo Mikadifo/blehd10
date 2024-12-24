65246e4ad9c78785345bee75afbf839876d24a05
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-12-24 14:19:34 -0500

    add: addLog function

--- index.js
+++ index.js
@@ -7,63 +7,78 @@ let characteristic = null;
 
 let $connectBtn = document.getElementById("connect-btn");
 let $disconnectBtn = document.getElementById("disconnect-btn");
-let $log = document.getElementById("log");
+let $log = document.getElementById("log-container");
 let $terminal = document.getElementById("terminal");
 
 $connectBtn.addEventListener("click", async (evt) => {
   try {
-    console.log("searching devices...");
+    addLog("Searching devices...");
 
     device = await navigator.bluetooth.requestDevice({
       filters: [{ services: [BLE_UUID] }],
     });
 
-    console.log(`connecting to ${device.name}`);
+    addLog(`connecting to ${device.name}`);
     server = await device.gatt.connect();
-    console.log("connected");
+    addLog("connected");
 
-    console.log("getting primary service");
+    addLog("getting primary service");
     const service = await server.getPrimaryService(BLE_UUID);
-    console.log("starting notifications");
+    addLog("starting notifications");
 
-    console.log("getting characteristic");
+    addLog("getting characteristic");
     characteristic = await service.getCharacteristic(CHAR_UUID);
 
-    console.log("starting notifications");
+    addLog("starting notifications");
     await characteristic.startNotifications();
     characteristic.addEventListener("characteristicvaluechanged", (event) => {
       const value = new TextDecoder().decode(event.target.value);
-      console.log(`Received: ${value}`);
+      addLog(`Received: ${value}`);
     });
 
-    console.log("Ready to send data.");
+    addLog("Ready to send data.");
+    $connectBtn.setAttribute("disabled", true);
+    $disconnectBtn.removeAttribute("disabled");
   } catch (error) {
-    console.log(error.message);
+    addLog(error.message);
   }
 });
 
 $disconnectBtn.addEventListener("click", (evt) => {
   if (device != null && device.gatt.connected) {
-    console.log("disconnecting...");
+    addLog("disconnecting...");
     device.gatt.disconnect();
-    console.log("disconnected");
+    addLog("disconnected");
+    $disconnectBtn.setAttribute("disabled", true);
+    $connectBtn.removeAttribute("disabled");
   }
 });
 
 $terminal.addEventListener("submit", async (evt) => {
   evt.preventDefault();
   if (device != null && device.gatt.connected) {
+    const $commandBtn = document.getElementById("command-btn");
     const data = new FormData(evt.target);
     const command = data.get("command");
 
-    console.log("sending data...");
-    console.log(command);
+    addLog(`Sending ${command}`);
 
     try {
       const encodedData = new TextEncoder().encode(command);
+      $commandBtn.setValue("Sending...");
       await characteristic.writeValue(encodedData);
     } catch (error) {
-      console.log(error.message);
+      addLog(error.message);
+    } finally {
+      $commandBtn.setValue("Send");
     }
+  } else {
+    addLog("Device not connected");
   }
 });
+
+const addLog = (msg) => {
+  const logMsg = document.createElement("p");
+  logMsg.innerText = msg;
+  $log.appendChild(logMsg);
+};
